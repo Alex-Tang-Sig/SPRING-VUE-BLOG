b509284d296086dc21fbe4f74257951ee8f9be34
alex-0914 <alextangting@gmail.com>
2022-05-23 10:21:48 +0800

    corsConfig done

---
+++ src/main/java/com/example/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CorsConfig class
+ *
+ * @author kejiwang
+ * @date 23/5/22
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+
+}
